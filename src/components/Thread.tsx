15243da372a7c31932facee0ca88449236edd553
vivaldev_ <149679225+valvira-gh@users.noreply.github.com>
2024-02-19 23:34:58 +0200

    initial commit

---
+++ src/components/Thread.tsx
@@ -0,0 +1,13 @@
+import React, { useOptimistic, useState, useRef } from 'react';
+import './styles.css';
+
+const Thread: React.FC = () => {
+
+    return (
+        <>
+            <div>Thread</div>
+        </>
+    )
+}
+
+export default Thread;
